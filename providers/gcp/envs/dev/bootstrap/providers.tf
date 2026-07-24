452fe25c5dab8cc137a89168033ad8de728ae03e
Matias Da Silva <matu.dasilva@gmail.com>
2026-07-24 11:23:11 -0300

    fix: isolate GCP budget quota provider

--- providers/gcp/envs/dev/bootstrap/providers.tf
+++ providers/gcp/envs/dev/bootstrap/providers.tf
@@ -13,3 +13,11 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+provider "google" {
+  alias                 = "budget"
+  project               = var.project_id
+  region                = var.region
+  billing_project       = var.project_id
+  user_project_override = true
+}
